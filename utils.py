11241212ffc514048ba73a14e30fd67516b0fd53
Noodles! <105514308+bamii-mudang@users.noreply.github.com>
2022-05-26 15:42:33 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,43 +2,46 @@ import os
 from venmo_api import Client, PaymentPrivacy
 from notifiers import get_notifier
 
+
 def get_env(env):
-  """
-  Verfies that an environment variable exists
-  and returns it.
+    """
+    Verfies that an environment variable exists
+    and returns it.
+    Exits script if not found.
+    """
+    if os.getenv(env):
+        print(f"✅ {env} is available in the environment.")
+        return os.getenv(env)
+    else:
+        print(f"❌ Can't find {env} in environment.")
+        print("   Exiting script. Please add and run again.")
+        quit()
 
-  Exits script if not found.
-  """
-  if os.getenv(env):
-      print(f"✅ {env} is available in the environment.")
-      return os.getenv(env)
-  else:
-      print(f"❌ Can't find {env} in environment.")
-      print("   Exiting script. Please add and run again.")
-      quit()
 
-env_vars = ["VENMO_ACCESS_TOKEN", "TELEGRAM_CHAT_ID", "TELEGRAM_BOT_TOKEN", "K_FRIEND_ID", "C_FRIEND_ID", "W_FRIEND_ID", "J_FRIEND_ID"]
+env_vars = ["VENMO_ACCESS_TOKEN", "GMAIL_APP_PASSWORD", "EMAIL_SENDER", "EMAIL_RECIPIENT", "E_FRIEND_ID"]
+
 
 def verify_env_vars(vars, numOfExpected):
-  """
-  Verifies the list of vars are defined in the environment.
-  """
+    """
+    Verifies the list of vars are defined in the environment.
+    """
 
-  availableEnvVars = []
+    availableEnvVars = []
 
-  for var in vars:
-    # If it returns the env, which would be True
-    # then we know it's available
-    if get_env(var):
-        availableEnvVars.append(var)
+    for var in vars:
+        # If it returns the env, which would be True
+        # then we know it's available
+        if get_env(var):
+            availableEnvVars.append(var)
 
-  if len(availableEnvVars) == numOfExpected:
-    return True
-  else:
-    # This will technically never run
-    # because if one doesn't exist, then get_env quits
-    # but adding here for posterity
-    return False
+    if len(availableEnvVars) == numOfExpected:
+        return True
+    else:
+        # This will technically never run
+        # because if one doesn't exist, then get_env quits
+        # but adding here for posterity
+        return False
+
 
 def get_env_vars(vars):
     """
@@ -51,6 +54,7 @@ def get_env_vars(vars):
 
     return allVars
 
+
 def get_month(now):
     """
     Returns the current month.
@@ -59,6 +63,7 @@ def get_month(now):
 
     month = now.strftime("%B")
     return month
+
 
 class Venmo:
     def __init__(self, access_token):
@@ -72,15 +77,18 @@ class Venmo:
             print("ERROR: user did not comeback. Check username.")
             return None
 
-    def request_money(self, id, amount, description, callback = None):
+    def request_money(self, id, amount, description, callback=None):
         # Returns a boolean: true if successfully requested
-        return self.client.payment.request_money(amount, description, id, PaymentPrivacy.PUBLIC, None, callback)
+        return self.client.payment.request_money(amount, description, id, PaymentPrivacy.PRIVATE, None, callback)
 
-class Telegram:
-    def __init__(self, bot_token, chat_id):
-        self.bot_token = bot_token
-        self.chat_id = chat_id
-        self.client = get_notifier('telegram')
 
-    def send_message(self, message):
-        self.client.notify(message=message, token=self.bot_token, chat_id=self.chat_id)
+class Gmail:
+    def __init__(self, from_address):
+        self.client = get_notifier('gmail')
+        self.client.defaults
+        {'subject': "New email from 'notifiers'!", 'from': from_address, 'host': 'smtp.gmail.com', 'port': 587,
+         'tls': True, 'ssl': False, 'html': False}
+
+    def send_message(self, recipient, username, app_password, subject, message):
+        self.client.notify(to=recipient, subject=subject, message=message,
+                           username=username, password=app_password)
